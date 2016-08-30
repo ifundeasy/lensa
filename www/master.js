cfe9cba61eb1eeb80553583262c1916dc7bfb6f8
go trial <microponizzied@yahoo.com>
2016-08-30 07:45:44 +0000

    tes commit dari c9

--- www/master.js
+++ www/master.js
@@ -1,6 +1,6 @@
 var env = process.env;
 
-var info = {
+var info = { // lalalalalalala tes push github
     version : env.npm_package_version,
     port : env.npm_package_port,
     name : env.npm_package_name,
