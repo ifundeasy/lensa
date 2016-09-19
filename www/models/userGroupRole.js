38536594195165482cb33b296e9c349500a8b62f
Afa Rappresent <mail.vudin@gmail.com>
2016-09-19 23:26:01 +0700

    no message

--- www/models/userGroupRole.js
+++ www/models/userGroupRole.js
@@ -1,35 +1,34 @@
 module.exports = function (mongoose) {
     var Schema = mongoose.Schema;
+    var routesSchema = new Schema({
+        model : {
+            type : String,
+            lowercase : true,
+            validate : {
+                validator : function (v) {
+                    var models = Object.keys(mongoose.models).map(function (key) {
+                        return mongoose.models[key].collection.name
+                    });
+                    return models.indexOf(v.toLowerCase()) > -1;
+                },
+                message : '{VALUE} is not a valid model'
+            },
+            required : true
+        },
+        methods : [{
+            type : String,
+            uppercase : true,
+            required : true,
+            enum : ["GET", "POST", "PUT", "DELETE"],
+            default : "GET"
+        }]
+    })
     var userGroupRoleSchema = new Schema({
         name : {
             type : String,
             required : true
         },
-        routes : [
-            {
-                model : {
-                    type : String,
-                    lowercase: true,
-                    validate : {
-                        validator : function (v) {
-                            var models = Object.keys(mongoose.models).map(function(key){
-                                return mongoose.models[key].collection.name
-                            });
-                            return models.indexOf(v.toLowerCase()) > -1;
-                        },
-                        message : '{VALUE} is not a valid model'
-                    },
-                    required : true
-                },
-                methods : [{
-                    type : String,
-                    uppercase: true,
-                    required : true,
-                    enum : ["GET", "POST", "PUT", "DELETE"],
-                    default : "GET"
-                }]
-            }
-        ],
+        routes : [routesSchema],
         notes : String,
         createdAt : {type : Date, default : Date.now},
         active : {type : Boolean, default : true}
