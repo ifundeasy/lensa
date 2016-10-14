99288c870247025a9d1cc85ee139a631c1d7d3d3
Falih Mulyana <falihakz@gmail.com>
2016-10-15 01:29:34 +0700

    Finishing up soap API (without auth)

--- www/routes/soap.js
+++ www/routes/soap.js
@@ -19,7 +19,8 @@ module.exports = function(args){
  		var class_route = req.header("Class");
 
  		switch(class_route){
- 			case 'register':
+
+ 			case 'user/create':
  				// check for mandatory field
  				if(req.body.first_name && req.body.username && req.body.email && req.body.password){
  					// check for username and email availability
@@ -87,18 +88,15 @@ module.exports = function(args){
  			case 'login':
  				break;
 
+ 			case 'logout':
+ 				break;
 
-
-
-
-
-
-
-
- 			case 'timeline':
+ 			case 'report/all':
  				var Post = Collection['posts'];
+ 				var start = req.body.start;
+ 				var limit = req.body.limit;
 
- 				Post.find().then(function(docs){
+ 				Post.find().sort('createdAt').then(function(docs){
  					var body = {
 			            "status": 1,
 			            "data": docs
@@ -115,11 +113,29 @@ module.exports = function(args){
  				});
  				break;
 
-
- 			case 'newreport':
+ 			case 'report/get':
  				var Post = Collection['posts'];
 
- 				var useridDummies = ['5800eada5cfa2608637c49c5','5800364438e91da4b2e74c74'];
+ 				Post.findOne({ _id: req.body._id}).then(function(docs){
+ 					var body = {
+			            "status": 1,
+			            "data": doc
+			        };
+			        res.status(200).send(body);
+ 				}).catch(function(e){
+ 					var body = {
+			            "status": 0,
+			            "message": e
+			        };
+
+			        res.status(500).send(body);
+ 				});
+ 				break;
+
+ 			case 'report/create':
+ 				var Post = Collection['posts'];
+
+ 				var useridDummies = ['5800eada5cfa2608637c49c5','5800364438e91da4b2e74c74', '5800f03cfc3d480d6f32e51a', '5800f01bfc3d480d6f32e519'];
  				var useridDummiesIndex = Math.floor(Math.random() * ((useridDummies.length-1) - 0 + 1)) + 0;
  				var newPost = new Post({
  					title: req.body.title,
@@ -149,6 +165,127 @@ module.exports = function(args){
  				});
 
  				break;
+
+
+ 			case 'report/delete':
+ 				var Post = Collection['posts'];
+
+ 				Post.findOne({_id: req.body._id}).remove().then(function(doc){
+ 					var body = {
+			            "status": 1,
+			            "data": doc
+			        };
+
+			        res.status(200).send(body);
+ 				}).catch(function(e){
+ 					var body = {
+			            "status": 0,
+			            "message": e
+			        };
+
+			        res.status(500).send(body);
+ 				});
+ 				break;
+
+ 			case 'comment/create':
+ 				break;
+
+
+ 			case 'comment/delete':
+ 				break;
+
+
+ 			case 'user/get':
+ 				var User = Collection['users'];
+ 				User.findOne({_id: req.body._id}).then(function(doc){
+ 					var body = {
+			            "status": 1,
+			            "data": doc
+			        };
+
+			        res.status(200).send(body);
+ 				}).catch(function(e){
+ 					var body = {
+			            "status": 0,
+			            "message": e
+			        };
+
+			        res.status(500).send(body);
+ 				});
+ 				break;
+
+ 			case 'user/update':
+ 				var User = Collection['users'];
+ 				User.findOne({_id: req.body._id}).then(function(doc){
+ 					if(Object.keys(doc).length != 0){
+ 						if(req.body.username) doc.username = req.body.username;
+ 						if(req.body.first_name) doc.name.first = req.body.first_name;
+ 						if(req.body.last_name) doc.name.last = req.body.last_name;
+ 						if(req.body.gender) doc.gender = req.body.gender;
+ 						if(req.body.password) doc.password = req.body.password;
+ 						if(req.body.email) doc.email = req.body.email;
+ 						if(req.body.phone) doc.phone = req.body.phone;
+ 						if(req.body.address) doc.address = req.body.address;
+ 						if(req.body.country) doc.country = req.body.country;
+ 						if(req.body.state) doc.state = req.body.state;
+ 						if(req.body.zipcode) doc.zipcode = req.body.zipcode;
+ 						if(req.body.birthDate) doc.birthDate = req.body.birthDate;
+
+ 						doc.save(function(err){
+ 							if(!err){
+ 								var body = {
+						            "status": 1,
+						            "message": "user data has been updated"
+						        };
+
+						        res.status(200).send(body);
+ 							} else {
+ 								var body = {
+						            "status": 0,
+						            "message": err
+						        };
+
+						        res.status(500).send(body);	
+ 							}
+ 						});
+ 					} else {
+ 						var body = {
+				            "status": 0,
+				            "message": "invalid user id"
+				        };
+
+				        res.status(500).send(body);
+ 					}
+ 				}).catch(function(e){
+ 					var body = {
+			            "status": 0,
+			            "message": e
+			        };
+
+			        res.status(500).send(body);
+ 				});
+ 				break;
+
+
+ 			case 'organization/get':
+ 				var Organization = Collection['organizations'];
+ 				Organization.findOne({_id: req.body._id}).then(function(doc){
+ 					var body = {
+			            "status": 1,
+			            "data": doc
+			        };
+
+			        res.status(201).send(body);
+ 				}).catch(function(e){
+ 					var body = {
+			            "status": 0,
+			            "message": e
+			        };
+
+			        res.status(500).send(body);
+ 				});
+ 				break;
+
  		}
 
  		
